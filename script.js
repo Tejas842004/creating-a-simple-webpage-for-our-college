7b08edf9ee4607d2662820b4c0d5dfa603dff4e0
Tejas T S <tejasts2004@gmail.com>
2025-10-05 18:53:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth Scroll for Navbar Links
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if(target) target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
